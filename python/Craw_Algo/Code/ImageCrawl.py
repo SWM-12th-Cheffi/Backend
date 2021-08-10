83340b124b2c8cb5cb03f5c9a938869dcc43bcbf
Nesquitto <ret0422@gmail.com>
2021-08-11 02:05:28 +0900

    test: Connection Mongo - Python

--- python/Craw_Algo/Code/ImageCrawl.py
+++ python/Craw_Algo/Code/ImageCrawl.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 import urllib.request
 from pymongo import MongoClient
 
-client = MongoClient('172.17.0.3', 27017)
+client = MongoClient('172.17.0.4', 27017)
 print(client.list_database_names())
 mydb = client['ImageData'] # test table
 #myIng = mydb['IngredData_Test'] # collection for Ingredient Data
 myCol = mydb['haemuk'] # collection for Recipe Data
 
-for i in range(1, 101):
+for i in range(1, 100):
     Url = 'https://haemukja.com/recipes?page=' + str(i)
     r = requests.get(Url, allow_redirects=False)
     serverStatus = True if r.status_code == 200 else False
@@ -19,9 +19,9 @@ for i in range(1, 101):
         image_Data = soup.select('#content > section > div.recipes > div > ul > li')
         for j in image_Data:
             imageID = j.find('a')['href'].split('/')[2]
-            urllib.request.urlretrieve(j.find('a').find('img')['src'], "../../../image/haemuk/"+j.find('a')['href'].split('/')[2]+".jpg")
+            urllib.request.urlretrieve(j.find('a').find('img')['src'], j.find('a')['href'].split('/')[2]+".jpg")
             result = myCol.insert_one({"id": imageID})
-#            print(result)
+            print(result)
     else:
         print("id: " + str(i) + " redirected")
 print("——————————————finish———————————————")

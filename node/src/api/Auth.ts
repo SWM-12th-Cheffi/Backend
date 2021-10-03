25e7e13442330effecbe96193ecea0f321c95eaa
Nesquitto <ret0422@gmail.com>
2021-10-03 23:04:59 +0900

    fix: Fix Small Error

--- node/src/api/Auth.ts
+++ node/src/api/Auth.ts
@@ -6,7 +6,7 @@ const debug = require('debug')('Cheffi:Auth');
 authRouter.post('/', async function (req, res) {
   console.log('Authorization Api Called');
   let authorizationToken: string = String(req.headers['authorization']).split(' ')[1];
-  let authorizationPlatform: string = String(req.headers['platform']).split(' ')[1];
+  let authorizationPlatform: string = String(req.headers['platform']);
   let returnStructure = await authz(authorizationToken, authorizationPlatform, -1);
   res.statusMessage = returnStructure.header.message;
   res.status(returnStructure.header.status).json(returnStructure.auth);

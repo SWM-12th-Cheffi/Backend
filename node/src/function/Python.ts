a7639dbe6aab027d1a8e3f0ff5883ddc33f1bb33
Nesquitto <ret0422@gmail.com>
2021-10-09 18:33:42 +0900

    feat: Change Python File

--- node/src/function/Python.ts
+++ node/src/function/Python.ts
@@ -1,13 +1,24 @@
-//Recc setting
 import axios from 'axios';
 var pyAddr: string = String(process.env.PYTHON_ADDR);
 
+// 레시피를 사용자에 맞게 추천해주는 함수
 export async function SortByRecc(reccObj: object) {
   console.log(reccObj);
   return axios({
     method: 'post',
     //server
-    url: pyAddr,
+    url: pyAddr + '/recommend',
+    data: reccObj,
+  });
+}
+
+// 저장된 유저 정보를 기반으로 선호도 데이터를 업데이트하는 함수
+export async function UpdateUserPreference(reccObj: object) {
+  console.log(reccObj);
+  return axios({
+    method: 'post',
+    //server
+    url: pyAddr + '/userpreference_update',
     data: reccObj,
   });
 }

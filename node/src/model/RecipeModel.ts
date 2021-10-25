2430e92ca84ddc5ba2ae8277a7ca82635ba1dc40
Nesquitto <ret0422@gmail.com>
2021-10-26 01:02:40 +0900

    feat: Modify Recipe Schema
    스크랩 수, 요약 스키마가 포함됨

--- node/src/model/RecipeModel.ts
+++ node/src/model/RecipeModel.ts
@@ -16,6 +16,7 @@ var RecipeSchema = new Schema(
     id: Schema.Types.ObjectID,
     recipeid: { type: Number, require: true, unique: true },
     title: { type: String, require: true },
+    summary: { type: String, require: false, default: '' },
     scrap: { type: Number, require: false, default: null }, // 만개의 레시피에는 없음
     time: { type: String, require: false },
     calories: { type: Number, require: false, default: null }, // 만개의 레시피에는 없음
@@ -23,6 +24,7 @@ var RecipeSchema = new Schema(
     difficulty: { type: String, require: false, default: '' }, // 해먹에는 없음
     ingredient: { type: Array, require: false, default: [] }, // 해먹 재료 데이터 업데이트 필요
     test: { type: Boolean, require: false },
+    innerscrap: { type: Number, require: true, default: 0 }, // 어플 내부에서 사용하는 스크랩 수
   },
   {
     versionKey: false,
